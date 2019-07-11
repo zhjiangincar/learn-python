aad3b4f10c1f024d633580b117f8290bcdaeafef
zhjiangincar <zhjiang@incarcloud.com>
2019-07-11 17:07:20 +0800

    Add samples

---
+++ samples/basic/hello.py
@@ -0,0 +1,5 @@
+## ------------ Learn Python  sample  --------------- ##
+## ------------ coding: utf-8                --------------- ##
+## ------------ 第一个 Python 程序 print
+
+print("Hello world!")
---
+++ samples/basic/hello2.py
@@ -0,0 +1,6 @@
+## ------------ Learn Python  sample  --------------- ##
+## ------------ coding: utf-8                --------------- ##
+## ------------ 测试输入与输出 input print
+
+name = input('Please input your name :')
+print('Hello,',name)
